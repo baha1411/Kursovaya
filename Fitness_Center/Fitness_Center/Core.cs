724c1f926bf9d2b0ccc370b172952fe332aaa7bd
baha1411 <orifsoevb@gmail.com>
2021-06-04 10:57:15 +0300

    Kursa4

---
+++ Fitness_Center/Fitness_Center/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness_Center
+{
+    class Core
+    {
+        public static fitness_kpEntities DB = new fitness_kpEntities();
+    }
+}
